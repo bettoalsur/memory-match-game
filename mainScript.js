192c864afca47a695037d0ffd1e19ebaa24b09c1
Alberto L. Duran <96275263+bettoalsur@users.noreply.github.com>
2022-02-02 23:05:30 -0300

    improved card animation and code organization

--- mainScript.js
+++ mainScript.js
@@ -8,23 +8,50 @@ while( num_cartas < 4 || num_cartas > 14 || num_cartas%2 != 0 ) {
     num_cartas = parseFloat(num_cartas);
 }
 
+// Cria variaveis do jogo
+
+var indexConteudo = [];
+var verificador = [];
+
+var compare = [null,null];
+var impedeSelecao = false;
+var contadorJogadas = 0;
+
+var cont = 0;
+for (let i = 0 ; i < num_cartas ; i += 2) {
+    indexConteudo.push(cont);
+    indexConteudo.push(cont);
+    verificador.push(true);
+    verificador.push(true);
+    cont ++;
+}
+
+indexConteudo.sort( function () {
+    return 0.5 - Math.random();
+} );
+
 // Cria as cartas HTML
 
 var aux = "";
 for (let i = 0; i < num_cartas ; i++) {
-    aux += ('<div id="carta'+i+'" class="carta" onclick="handle_click(this)"></div>');
+
+    var logo = indexConteudo[i];
+
+    aux += `
+    <div id="carta${i}" class="carta" onclick="handle_click(this)" >
+        <div class="verso face">
+            <img src="files/front.png" alt="">
+        </div>
+        <div class="frente face virada">
+            <img src="files/logo${logo}.svg" alt="">
+        </div>
+    </div>
+    `;
 }
 
 var elemento = document.querySelector(".cartas");
 elemento.innerHTML = aux;
 
-// Coloca a imagem do verso das cartas
-
-var cartas = elemento.children;
-
-for (let carta of cartas) {
-    carta.innerHTML = "<img src='files/front.png'>"
-}
 
 // Centraliza as cartas na janela
 
@@ -51,44 +78,20 @@ window.addEventListener("resize", function () {
 // Logica do jogo //
 ////////////////////
 
-// Cria os indices para as cartas e as baralha
-
-var indexCards = [];
-var compare = [null,null];
-var impedeSelecao = false;
-var contadorJogadas = 0;
-
-var cont = 0;
-for (let i = 0 ; i < num_cartas ; i += 2) {
-    indexCards.push(cont);
-    indexCards.push(cont);
-    cont ++;
-}
-
-indexCards.sort( function () {
-    return .5 - Math.random();
-} );
-
-// cria array com booleanos que verificam se a carta ainda pode ser virada
-
-var verificadores = [];
-for (let i = 0 ; i < num_cartas ; i ++) {
-    verificadores.push(true);
-}
-
 // gestiona o click nas cartas
 
 function handle_click(elemento) {
 
     var num = parseFloat( elemento.id.replace('carta','') );
 
-    if ( verificadores[num] && !impedeSelecao ) {
-        verificadores[num] = false;
+    if ( verificador[num] && !impedeSelecao ) {
+
+        // impede temporalmente a selecao da carta
+        verificador[num] = false;
 
         // vira a carta
-        var index = indexCards[num];
-        animarVirarCarta(elemento,index);
-
+        virarCarta(elemento);
+        
         // armazena a carta no comparador
         if (compare[0] == null) {
             compare[0] = num;
@@ -96,38 +99,44 @@ function handle_click(elemento) {
             compare[1] = num;
         } 
 
-        // compara as cartas se o comparador estiver cheio
-        if ( compare[0] != null && compare[1] != null ) {
-            impedeSelecao = true;
-            setTimeout(compararCartas,1000);
-        }
-
         // aumenta o numero de jogadas em 1
         contadorJogadas++;
+
+        // compara as cartas se o comparador estiver cheio
+        if ( compare[0] != null && compare[1] != null ) {
+            impedeSelecao = true; 
+            setTimeout(compararCartas, 1.00 * 1000);
+        }
 
     }
 
 }
 
+function virarCarta(elemento) {
+    elemento.querySelector(".verso").classList.toggle("virada");
+    elemento.querySelector(".frente").classList.toggle("virada");
+}
+
 function compararCartas(){
 
-    if ( indexCards[compare[0]] == indexCards[compare[1]] ) {
+    if ( indexConteudo[compare[0]] == indexConteudo[compare[1]] ) {
         // cartas iguais 
 
-        document.querySelector("#carta"+compare[0]).style.borderColor = "#32B72F";
-        document.querySelector("#carta"+compare[1]).style.borderColor = "#32B72F";
+        for (let i = 0 ; i < compare.length ; i++) {
+            elemento = document.querySelector("#carta"+compare[i]);
+            elemento.querySelector(".frente").classList.add("encontrada");
+        }
+        
         verificarFimJogo();
 
     } else {
         // cartas diferentes
 
-        verificadores[compare[0]] = true;
-        var elemento = document.querySelector("#carta"+compare[0]);
-        animarDesvirarCarta(elemento);
-
-        verificadores[compare[1]] = true;
-        var elemento = document.querySelector("#carta"+compare[1]);
-        animarDesvirarCarta(elemento);
+        for (let i = 0 ; i < compare.length ; i++) {
+            verificador[compare[i]] = true;
+            var elemento = document.querySelector("#carta"+compare[i]);
+            virarCarta(elemento);
+        }
 
     }
 
@@ -142,41 +151,14 @@ function compararCartas(){
 function verificarFimJogo() {
 
     var cont = 0;
-    for (let i = 0 ; i < verificadores.length ; i++ ){
-        if (!verificadores[i]) {
+    for (let i = 0 ; i < verificador.length ; i++ ){
+        if (!verificador[i]) {
             cont++;
         }
     }
 
-    if (cont == verificadores.length) {
+    if (cont == verificador.length) {
         var mensagem = "Você ganhou em "+contadorJogadas+" jogadas!";
-        setTimeout( alert(mensagem) , 2000 );
+        alert(mensagem);
     }
-}
-
-
-// animacao para virar a carta
-
-function animarVirarCarta(elemento,index) {
-    elemento.style.transform = "rotateY(-90deg)";
-    setTimeout(animarVirarCartaP2(elemento,index),250);
-}
-
-function animarVirarCartaP2(elemento,index) {
-    elemento.querySelector("img").style.content = "url('files/logo"+index+".svg')";
-    elemento.querySelector("img").style.transform = "rotateY(-180deg)";
-    elemento.style.transform = "rotateY(-180deg)";
-}
-
-// animacao para des-virar a carta
-
-function animarDesvirarCarta(elemento) {
-    elemento.style.transform = "rotateY(-90deg)";
-    setTimeout(animarDesvirarCartaP2(elemento),250);
-}
-
-function animarDesvirarCartaP2(elemento) {
-    elemento.querySelector("img").style.content = "url('files/front.png')";
-    elemento.querySelector("img").style.transform = "rotateY(0deg)";
-    elemento.style.transform = "rotateY(0deg)";
 }
